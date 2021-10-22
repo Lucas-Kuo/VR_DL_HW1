5fdb32647dcb7b8717fc1ecd6ba24e3bf8053d07
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-10-22 21:44:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ import os
 # initialize the path to the *original* input directory of images
 ORIG_INPUT_DATASET = "training_images"
 
+TRAIN_LABEL_PATH = "train_labels.txt"
+
 # initialize the base path to the *new* directory that will contain
 # our images after computing the training and testing split
 BASE_PATH = "dataset"
