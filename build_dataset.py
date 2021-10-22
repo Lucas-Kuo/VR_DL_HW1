a8226255af8f58af7220abf3595cd5e297cda107
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-10-22 20:59:53 +0800

    Update build_dataset.py

--- build_dataset.py
+++ build_dataset.py
@@ -2,7 +2,16 @@
 import config
 from imutils import paths
 import shutil
+import random
 import os
+
+# grab the paths to all input images in the original input directory zipped with their respective label and shuffle them
+imagePaths = list(paths.list_images(config.ORIG_INPUT_DATASET))
+training_labels = []
+with open()
+random.seed(42)
+random.shuffle(imagePaths)
+
 
 # loop over the data splits
 for split in (config.TRAIN, config.VAL):
