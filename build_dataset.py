c94458b572e87ee7e1ec9f695d4cc88c7f142d14
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-10-22 22:13:33 +0800

    Update build_dataset.py

--- build_dataset.py
+++ build_dataset.py
@@ -5,30 +5,39 @@ import shutil
 import random
 import os
 
-# grab the paths to all input images in the original input directory zipped with their respective label and shuffle them
+# grab the paths to all input images in the original input directory and shuffle them
 imagePaths = list(paths.list_images(config.ORIG_INPUT_DATASET))
 training_labels = []
 with open(config.TRAIN_LABEL_PATH) as f:
 	for line in f:
 		training_labels.append(line)
-zippedPaths = list(zip(imagePaths, training_labels))
+
 random.seed(42)
-random.shuffle(zippedPaths)
+random.shuffle(imagePaths)
+
+# key: image_name
+# value: label
+filename_class_pair = {}
+
+for training_label in training_labels:
+	filename, label = training_label.split()
+	filename_class_pair[filename] = label
+	
 
 # we'll be using part of the training data for validation
-i = int(len(zippedPaths) * config.VAL_SPLIT)
-val_zippedPaths = zippedPaths[:i]
-train_zippedPaths = zippedPaths[i:]
+i = int(len(imagePaths) * config.VAL_SPLIT)
+val_Paths = imagePaths[:i]
+train_Paths = imagePaths[i:]
 
 
 # define the datasets that we'll be building
 datasets = [
-	("training", train_zippedPaths, config.TRAIN_PATH),
-	("validation", val_zippedPaths, config.VAL_PATH),
+	("training", train_Paths, config.TRAIN_PATH),
+	("validation", val_Paths, config.VAL_PATH),
 ]
 
 # loop over the datasets
-for (dType, zippedPaths, baseOutput) in datasets:
+for (dType, imagePaths, baseOutput) in datasets:
 	# show which data split we are creating
 	print("[INFO] building '{}' split".format(dType))
 	
@@ -38,14 +47,12 @@ for (dType, zippedPaths, baseOutput) in datasets:
 		os.makedirs(baseOutput)
 
 	# loop over the input image paths
-	for zippedPath in zippedPaths:
-		# extract the image path and its label
-		imagePath = zippedPath[0]
-		label_name = zippedPath[1]
-		#imagePath, label_name = zip(*zippedPath)
-		
+	for imagePath in imagePaths:
+
 		# extract the filename of the input image
 		filename = imagePath.split(os.path.sep)[-1]
+		
+		label_name = filename_class_pair[filename]
 		
 		# build the path to the label directory
 		label_directory = os.path.sep.join([baseOutput, label_name])
