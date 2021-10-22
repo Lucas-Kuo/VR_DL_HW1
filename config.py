6baa54e30a813dff915b886e0877523a9c81876c
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-10-22 15:38:50 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# inport necessary packages
+import os
+
+# initialize the path to the *original* input directory of images
+
+
+# initialize the base path to the *new* directory that will contain
+# our images after computing the training and testing split
+BASE_PATH = "dataset"
+
+# define the names of the training, testing, and validation
+# directories
+TRAIN = "training"
+TEST = "evaluation"
+VAL = "validation"
+
+
+
+# set the batch size
+BATCH_SIZE = 32
