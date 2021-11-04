4c12c02d3d576aaf6b67a47655ebbaa1cd05cf10
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-11-04 22:41:55 +0800

    Update build_dataset2.py

--- build_dataset2.py
+++ build_dataset2.py
@@ -7,7 +7,8 @@ import os
 
 random.seed(42)
 
-# grab the paths to all input images in the original input directory and shuffle them
+# grab the paths to all input images
+# in the original input directory and shuffle them
 imagePaths = list(paths.list_images(config.ORIG_INPUT_DATASET))
 
 
@@ -15,7 +16,7 @@ imagePaths = list(paths.list_images(config.ORIG_INPUT_DATASET))
 training_labels = []
 with open(config.TRAIN_LABEL_PATH) as f:
     for line in f:
-        line = line[:-1] # take away the trailing \n
+        line = line[:-1]  # take away the trailing \n
         training_labels.append(line)
 
 # key: label/class name
@@ -40,17 +41,17 @@ for class_name in class_file_pairs:
 print("[INFO] building dataset...")
 for class_name in class_file_pairs:
     # on average, there should be 15 images for each class
-    # therefore, we split 2 of them(~15%) for validation and the rest for training
+    # we split 2 of them(~15%) for validation and the rest for training
     for imageName in class_file_pairs[class_name][:2]:
         # directories for the image source and destination
         save_dir = os.sep.join([config.VAL_PATH, class_name])
         source_dir = os.sep.join([config.ORIG_INPUT_DATASET, imageName])
-        
+
         # in case the config.py didn't build the directory, check again
         if not os.path.exists(save_dir):
             print("[INFO] creating '{}' directory".format(save_dir))
             os.makedirs(save_dir)
-            
+
         # copy the image
         p = os.sep.join([save_dir, imageName])
         shutil.copy2(source_dir, p)
