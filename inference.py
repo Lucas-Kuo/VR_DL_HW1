cfbdbaf5e86965a8c623433cad7050ab4e4c3272
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-11-04 21:21:46 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,54 +1,52 @@
 import numpy as np
 import os
 import tensorflow as tf
-from tensorflow.data import AUTOTUNE
-from tensorflow.keras.layers.experimental.preprocessing import RandomRotation
-from tensorflow.keras.applications.efficientnet import preprocess_input
+import gdown
+
 from imutils import paths
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 import config
 from train_model import build_model, weight_path
 
-
-# for unlabeled images, i.e. testing/evaluation dataset
 def load_images(imagePath):
-	# read the image from disk, decode it, convert the data type to
-	# floating point, and resize it
-	image = tf.io.read_file(imagePath)
-	image = tf.image.decode_png(image, channels=3)
-	image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-	image = tf.image.resize(image, config.IMAGE_SIZE)
-    
-	# no label is available for testing dataset
-	label = None
-	
-	# return the image and the label
-	return (image, label)
+	# pass in the path of testing dataset
+	return image_dataset_from_directory(imagePath, labels=None, shuffle=False, label_mode=None, 
+                                        batch_size=config.BATCH_SIZE, image_size=config.IMG_SIZE)
 
-# grab all testing dataset image paths
-testPaths = list(paths.list_images(config.TEST_PATH))
+# load testing dataset
+test_dataset = load_images(config.TEST_PATH)
 
-# build the testing dataset and data input pipeline
-test_dataset = tf.data.Dataset.from_tensor_slices(testPaths)
-test_dataset = (test_dataset
-	.map(load_images, num_parallel_calls=AUTOTUNE)
-	.cache()
-	.batch(config.BATCH_SIZE)
-	.prefetch(AUTOTUNE)
-)
-
-# build our model and retrieve the weights we saved
-model = build_model()
-model.load_weights(weight_path)
+# download the trained model from google drive
+gdown.download(config.MODEL_URL, config.MODEL_NAME, quiet=False)
+model = tf.keras.models.load_model(config.MODEL_NAME)
 
 # make our inference
 predictions = model.predict(test_dataset)
 
-# to be modified
-largest_confidence = list(np.argmax(predictions, axis = 1))
+# predictions are 200-dimentional vectors
+# we only keep the largest ones' indices
+output = list(np.argmax(predictions, axis = 1))
+
+# map the image file names to their respective result
 result = {}
-N = len(largest_confidence)
+N = len(output)
 for i in range(N):
-  name = test_dataset.file_paths[i][-8:] # the file path has the format: .../.../xxxx.jpg
-  label = CLASSES[output[i]]
-  result[name] = label
+    name = test_dataset.file_paths[i][-8:]
+    label = config.CLASSES[output[i]]
+    result[name] = label
+
+# the retrieve the order of sample answer
+evaluation_filenames = []
+with open(config.SAMPLE_ANSWER_PATH, "r") as f:
+    for line in f:
+        # the file name and class name is split
+        # by a space
+        filename = line.split()[0]
+        evaluation_filenames.append(filename)
+ 
+# output the result
+with open("answer.txt", "w") as f:
+    for filename in evaluation_filenames:
+        s = filename + ' ' + result[filename] + '\n'
+        f.write(s)
