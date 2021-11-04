3f9ef2e0bfcdaba710a355762263132c1267accd
Lucas Kuo <74166753+Lucas-Kuo@users.noreply.github.com>
2021-11-04 22:39:29 +0800

    Update build_dataset.py

--- build_dataset.py
+++ build_dataset.py
@@ -5,7 +5,8 @@ import shutil
 import random
 import os
 
-# grab the paths to all input images in the original input directory and shuffle them
+# grab the paths to all input images
+# in the original input directory and shuffle them
 imagePaths = list(paths.list_images(config.ORIG_INPUT_DATASET))
 
 random.seed(42)
@@ -26,7 +27,6 @@ for training_label in training_labels:
     # so we split the string with a whitespace
     filename, label = training_label.split()
     filename_class_pair[filename] = label
-	
 
 # we'll be using part of the training data for validation
 i = int(len(imagePaths) * config.VAL_SPLIT)
@@ -44,7 +44,7 @@ datasets = [
 for (dType, imagePaths, baseOutput) in datasets:
     # show which data split we are creating
     print("[INFO] building '{}' split".format(dType))
-    
+
     # if the output base output directory does not exist, create it
     if not os.path.exists(baseOutput):
         print("[INFO] creating '{}' directory".format(baseOutput))
@@ -55,22 +55,19 @@ for (dType, imagePaths, baseOutput) in datasets:
 
         # extract the filename of the input image
         filename = imagePath.split(os.path.sep)[-1]
-        
+
         # check the label of this image
         label_name = filename_class_pair[filename]
-        
+
         # build the path to the label directory
         label_directory = os.path.sep.join([baseOutput, label_name])
-        
+
         # if the label output directory does not exist, create it
         if not os.path.exists(label_directory):
             print("[INFO] creating '{}' directory".format(label_directory))
             os.makedirs(label_directory)
-        
+
         # construct the path to the destination image and then copy
         # the image itself
         p = os.path.sep.join([label_directory, filename])
         shutil.copy2(imagePath, p)
-
-
-
